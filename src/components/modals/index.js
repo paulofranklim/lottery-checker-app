1f533bf8ccf60a2adfb743eba2dc6c41623780e9
Paulo Franklim <paulofranklim@hotmail.com>
2021-09-03 20:30:49 -0300

    Confirmation modal working after insert a new user

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -82,7 +82,9 @@ export const ModalNewUser = (props) => {
     const [name, setName] = useState("")
     const [mail, setMail] = useState("")
     const [userName, setUserName] = useState("");
+    const [tempUserName, setTempUserName] = useState("")
     const [password, setPassword] = useState("");
+    const [showInfoModal, setShowInfoModal] = useState(false)
 
     async function handleCreteUser(e) {
         e.preventDefault()
@@ -99,50 +101,62 @@ export const ModalNewUser = (props) => {
         if (response) {
             setName("")
             setMail("")
+            setTempUserName(userName)
             setUserName("")
             setPassword("")
+            closeModalFunction()
+            setShowInfoModal(true)
         }
+
     }
 
     return (
-        <Modal show={show} onHide={() => closeModalFunction()} centered>
-            <Modal.Header>
-                <Modal.Title>{title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form style={{ margin: 20 }} onSubmit={e => handleCreteUser(e)}>
-                    <Form.Row>
-                        <Form.Group as={Col} >
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control value={name} onChange={e => setName(e.target.value)} required placeholder="Insert name" />
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} md="6" >
-                            <Form.Label>Login</Form.Label>
-                            <Form.Control value={userName} onChange={e => setUserName(e.target.value)} required placeholder="Insert user name" />
-                        </Form.Group>
-                        <Form.Group as={Col} md="6">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Insert password" />
-                        </Form.Group>
-                    </Form.Row>
-                    <Form.Row>
-                        <Form.Group as={Col} >
-                            <Form.Label>Mail</Form.Label>
-                            <Form.Control value={mail} onChange={e => setMail(e.target.value)} required placeholder="Insert mail" />
-                        </Form.Group>
-                    </Form.Row>
-                    <Button variant="primary" type="submit">
-                        Create
-                    </Button>
-                    <Button variant="danger" onClick={() => closeModalFunction()}>
-                        Close
-                    </Button>
-                </Form>
-            </Modal.Body>
+        <div>
+            <Modal show={show} onHide={() => closeModalFunction()} centered>
+                <Modal.Header>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form style={{ margin: 20 }} onSubmit={e => handleCreteUser(e)}>
+                        <Form.Row>
+                            <Form.Group as={Col} >
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control value={name} onChange={e => setName(e.target.value)} required placeholder="Insert name" />
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} md="6" >
+                                <Form.Label>Login</Form.Label>
+                                <Form.Control value={userName} onChange={e => setUserName(e.target.value)} required placeholder="Insert user name" />
+                            </Form.Group>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Insert password" />
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} >
+                                <Form.Label>Mail</Form.Label>
+                                <Form.Control value={mail} onChange={e => setMail(e.target.value)} required placeholder="Insert mail" />
+                            </Form.Group>
+                        </Form.Row>
+                        <Button variant="primary" type="submit">
+                            Create
+                        </Button>
+                        <Button variant="danger" onClick={() => closeModalFunction()}>
+                            Close
+                        </Button>
+                    </Form>
+                </Modal.Body>
 
-        </Modal>
+            </Modal>
+            <ModalInfo
+                show={showInfoModal}
+                closeModalFunction={() => setShowInfoModal(false)}
+                title="User created!"
+                message={'The user "' + tempUserName + '" is created.'}
+            />
+        </div>
     )
 }
 
